688e892cfe3eefe406ede3f1b3f1ae4097d6c206
Narayandwivedi <desinplus1@gmail.com>
2026-06-16 03:11:12 +0530

    improve ui

--- frontend/src/pages/PremiumCalculator.jsx
+++ frontend/src/pages/PremiumCalculator.jsx
@@ -424,6 +424,7 @@ const PremiumCalculator = () => {
                           <option value='100'>100</option>
                           <option value='150'>150</option>
                           <option value='200'>200</option>
+                          <option value='250'>250</option>
                           <option value='300'>300</option>
                         </select>
                       </div>
